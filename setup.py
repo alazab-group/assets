43aaf3f2d5068a7ec1e87a7ec1b25a56aa142605
Mohamed Azab <159525429+mohamedazab224@users.noreply.github.com>
2025-06-22 11:02:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
+import os
+import sys
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
-
-# get version from __version__ variable in assets/__init__.py
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "assets"))
 from assets import __version__ as version
 
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
 setup(
-	name="assets",
-	version=version,
-	description="Asset Management App for Alazab ERP",
-	author="Alazab Team",
-	author_email="info@alazab.com",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name="assets",
+    version=version,
+    description="Asset Management App for Alazab ERP",
+    author="Alazab Team",
+    author_email="info@alazab.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
 )
